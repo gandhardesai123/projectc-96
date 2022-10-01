7ef4a5fc036ecce205f66ff07784cf51513232df
gandhardesai123 <106746203+gandhardesai123@users.noreply.github.com>
2022-10-01 14:59:29 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,14 @@
+
+function addUser() {
+ 
+    //Set "player1_name" using localStorage.setItem() function
+	  //Set "player2_name" using localStorage.setItem() function
+    localStorage.setItem("player1_name", player1_name);
+    localStorage.setItem("player2_name", player2_name);
+    
+    window.location = "game_page.html";
+}
+function setItem() {
+  player
+
+}
